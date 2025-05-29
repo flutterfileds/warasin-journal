0fb1b67ac96fe6b1836927ffadd83abcd303ee58
jovita ayu <jovitaayr@gmail.com>
2025-05-30 01:08:26 +0700

    fix: text_cleaning

--- app/utils/text_cleaning.py
+++ app/utils/text_cleaning.py
@@ -1,10 +1,8 @@
 import re
 import string
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
 def clean_text(text):
@@ -16,7 +14,6 @@ def clean_text(text):
     text = re.sub(r'(\w)\1{2,}', r'\1', text)
 
     tokens = word_tokenize(text)
-    tokens = [word for word in tokens if word not in stop_words]
     lemmatized_tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
     return " ".join(lemmatized_tokens)
